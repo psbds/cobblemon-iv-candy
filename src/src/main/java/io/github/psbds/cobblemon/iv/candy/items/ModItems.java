aeca4f564c6d306c155085799f98a63b654abcce
psbds <psbds_filho@hotmail.com>
2025-07-26 23:35:53 -0300

    fix: removing item registration from static contexts

--- src/src/main/java/io/github/psbds/cobblemon/iv/candy/items/ModItems.java
+++ src/src/main/java/io/github/psbds/cobblemon/iv/candy/items/ModItems.java
@@ -1,11 +1,8 @@
 package io.github.psbds.cobblemon.iv.candy.items;
 
 import io.github.psbds.cobblemon.iv.candy.Boot;
-import io.github.psbds.cobblemon.iv.candy.items.components.DataShard;
-import io.github.psbds.cobblemon.iv.candy.items.components.DataCandy;
 import io.github.psbds.cobblemon.iv.candy.items.objects.candies.Candy;
 import io.github.psbds.cobblemon.iv.candy.items.objects.candies.CandyFactory;
-import io.github.psbds.cobblemon.iv.candy.items.objects.iv_extractor.DataIVExtractor;
 import io.github.psbds.cobblemon.iv.candy.items.objects.iv_extractor.IVExtractor;
 import io.github.psbds.cobblemon.iv.candy.items.objects.iv_extractor.IVExtractorFactory;
 import io.github.psbds.cobblemon.iv.candy.items.objects.shards.Shard;
@@ -17,11 +14,9 @@ import net.minecraft.world.item.Item;
 
 public class ModItems {
 
-    public static final Item CANDY = registerItem(Candy.NAME, CandyFactory.createDefault());
-
-    public static final Item SHARD = registerItem(Shard.NAME, ShardFactory.createDefault());
-
-    public static final Item IV_EXTRACTOR = registerItem(IVExtractor.NAME, IVExtractorFactory.createDefault());
+    public static Item CANDY;
+    public static Item SHARD;
+    public static Item IV_EXTRACTOR;
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Boot.MOD_ID, name),
@@ -29,8 +24,13 @@ public class ModItems {
     }
 
     public static void initialize() {
-        DataCandy.initialize();
-        DataShard.initialize();
-        DataIVExtractor.initialize();
+        Boot.LOGGER.info("Registering items for " + Boot.MOD_ID);
+        
+        // Register items using the factories
+        CANDY = registerItem(Candy.NAME, CandyFactory.createDefault());
+        SHARD = registerItem(Shard.NAME, ShardFactory.createDefault());
+        IV_EXTRACTOR = registerItem(IVExtractor.NAME, IVExtractorFactory.createDefault());
+        
+        Boot.LOGGER.info("Successfully registered items for " + Boot.MOD_ID);
     }
 }
